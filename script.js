371b077b5c537f2d229135a9c946bb0fc8dbb8ba
lbilde <ljb@easv.dk>
2017-01-31 13:50:25 +0100

    adding lcalstorage and users

--- script.js
+++ script.js
@@ -1,30 +1,74 @@
-$(document).ready(function() {
-   $('#toogle-create').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-   });
-   
-   $('#toogle-signin').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-   });
-   
-   $('#logon').click(function(){
-       var user = $('#name').val();
-       var pw = $('#pw').val();
-       console.log('user', user);
-       console.log('pw', pw);
-        if(user === 'ljb' && pw === 'ost'){
-            alert('U the king');
-        } else{
+$(document).ready(function () {
+    $('#toogle-create').click(function () {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+        return false;
+    });
+
+    $('#toogle-signin').click(function () {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+        return false;
+    });
+
+    $('#logon').click(function () {
+        var user = $('#name').val();
+        var pw = $('#pw').val();
+        console.log('user', user);
+        console.log('pw', pw);
+        if (user === 'ljb' && pw === 'ost') {
+            localStorage.setItem("loggedInUser", JSON.stringify({ user: user, pw: pw }));
+            logInCheck();
+        } else {
             alert('U not him! Please gief Lars!!')
+            return false;
         }
-   });
 
-   $('#forgot').click(function(){
-       alert('u forgetttttted itte');
-   });
+    });
 
-   $('#create').click(function(){
-       alert('u cannot create a user.. Muahhahahah');
-   });
-   
+    $('#logout').click(function () {
+        localStorage.setItem("loggedInUser", null);
+        logInCheck();
+    });
+
+    $('#forgot').click(function () {
+        alert('u forgetttttted itte');
+        return false;
+    });
+
+    $('#create').click(function () {
+        var user = { name: $('#create-name').val(), email: $('#create-email').val(), pw: $('#create-pw').val() }
+        var users = JSON.parse(localStorage.getItem("users"));
+        if (users === null) {
+            users = [];
+        }
+        users.push(user);
+        console.log('users: ', users);
+        localStorage.setItem("users", JSON.stringify(users));
+        return false;
+    });
+
+
+    function logInCheck() {
+        var localUsers = JSON.parse(localStorage.getItem("users"));
+        console.log('usies', localUsers);
+        var loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+        console.log('loggedInUser', loggedInUser);
+        if (loggedInUser != null) {
+            $('.loggedin-page').show();
+            $('.login-page').hide();
+            if(localUsers != null){
+                var userList = $("#userlist");
+                userList.empty();
+                for(i=0; i<localUsers.length; i++){
+                    $("<li class=\"message\"><a >"+localUsers[i].name+"</a></li>").appendTo(userList);
+                }
+            }
+           
+        } else {
+            $('.loggedin-page').hide();
+            $('.login-page').show();
+
+        }
+    }
+
+    logInCheck();
 });
